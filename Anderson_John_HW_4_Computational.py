9f493914886fea0c9c722a113d91a19b4ca87233
John Anderson <nhojanderson8@gmail.com>
2020-10-12 13:35:18 -0500

    Add files via upload
    Added part 2, Neuman BCs. I went ahead and made a new funtion for the Helmotz Thomas Algorithm function, rather than modify the first one with condition statements. I did this so that the function closely follows the psuedo code. I also changed the interval function back to how I originally had it because I need to approximate the u(x=0) value for the Neumann problem. I also added some print statements and enabled the outter for loop for the running the different k values.
    
    Now I just gotta do the plotting and tables. I am not sure if I will add this stuff to the code with conditional statements or if I will just use my functions and make the plots in the console.

--- Anderson_John_HW_4_Computational.py
+++ Anderson_John_HW_4_Computational.py
@@ -17,6 +17,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from math import sinh as sinh
+from math import cosh as cosh
 from math import log2 as log2
 from random import randint as randi
 
@@ -42,10 +43,15 @@ N_initial = N
 def DIF(L, N):
 #Discretizing the interval length. This is the same for both problems
     h = L/(N+1)
-    x = np.linspace(h, L-h, N)
+    x = np.linspace(0, L, N+2)
     return(x[:],h)
+# changed this back to how I originally had it because for the Neuman problem I need to 
+#approximate u(x=0)    
 
-#Helmholtz Thomas Algorith Function    
+#Helmholtz Thomas Algorith Function
+#do I wanna modify this slightly or make a whole new function for Nueman condition at x=0?
+#just make a new one for now, modify it later
+      
 def HTAF(N, h, lamda, U_o, A):   
 #inputs are N, lamda, U_o=u(x=0), and for this problem A.
 #Pre Thomas algorith set up. for this problem these values are all constant
@@ -70,69 +76,149 @@ def HTAF(N, h, lamda, U_o, A):
     for m in range(1,N):
         u_appx[-1-m] = (g[-1-m]-c*u_appx[-m])/alpha[-1-m]
     return(u_appx)
+    
+#for Neumann    
+def NHTAF(N, h, lamda, v, A):   
+#Pre Thomas algorith set up. for this problem these values are all constant
+#I now need to make N one point larger to incorporate the ghost node method for
+#u(x=0) whihch is unknown
+#but I am still keep h the same    
+    N=N+1    
+    a =-(2-lamda*h**2)
+    b = 1
+#I now need c to be a list because they are now not all the same
+#or I could use some conditinal statements but I wanna still follow the
+#pseudo code for the algorithm closely    
+    c = [1]*N
+    c[0]=2
+    f=A*h**2    
+#this line is added because of the ghost node method
+#right hand side, initial equation    
+    rhs_o = A*h**2+2*h*v
+    alpha = [0]*N
+    g = [0]*N
+    u_appx = [0]*N
+#following the psuedo code
+#zeroth element of this list corresponds to the first subscript in thomas algorith
+    alpha[0] = a
+    g[0] = rhs_o
+    for j in range(1, N):
+        alpha[j] = a-(b/alpha[j-1])*c[j-1]
+        g[j] = f-(b/alpha[j-1])*g[j-1]   
+    u_appx[N-1] = g[N-1]/alpha[N-1]
+    for m in range(1,N):
+        u_appx[-1-m] = (g[-1-m]-c[-1-m]*u_appx[-m])/alpha[-1-m]
+    return(u_appx)    
 
 #u exact function, for the helmhotlz dirchlet part 1 problem
        
 def uEF(k, L, x, A, U_o):
     u_exact = [((sinh(k*(L-x))+sinh(k*x))/sinh(k*L)-1)*A/k**2+
-U_o*sinh(k*(L-x))/sinh(k*L) for x in x]
+U_o*sinh(k*(L-x))/sinh(k*L) for x in x[1:-1]]
+#x[1:-1] I dont need u(x=0) or u(x=L) beacause they are given
     return(u_exact)
-
+    
+def uEF2(k, L, x, A, v):
+    u2_exact = [((cosh(k*x)/cosh(k*L))-1)*(A/k**2)
+-(v/k)*(sinh(k*(L-x))/cosh(k*L)) for x in x[0:-1]]
+#x[0:-1]  I dont need  u(x=L) beacause it is given    
+    return(u2_exact)
 #Calling the Discr. the Interval right here for now     
 x,h = DIF(L,N)   
  
-#im gonna need this eventually    
-#lenK=len(K)
-#for n in range(lenK)
-#    k = K[n]
+
+#Outter for lop for the different k values
+lenK=len(K)
+for n in range(lenK):
+    k = K[n]
 
 #just gonna start with k=1 for now
-k = K[0]
-
+    k = K[0]
+    print('For k = %s \n'%(k))
+    
 #Note: lamda in the helmholtz eq defined here
-lamda = -k**2
-
+    lamda = -k**2
+    
 #Part 1, Dirchlet
-
+    print('Part 1. Dirchlet Boundary conditions \n')
+    
 #Grid Convergence
-
+    
 #im using the flag so I dont have to call the function before and inside the while statement
-Flag = 0
+    Flag = 0
 #Difference between N and 2N variable
-Diff_N2N = 1*10**-3
-
-while Flag == 0:
+    Diff_N2N = 1*10**-3
+    
+    while Flag == 0:
 #comparing values near the middle of the interval now
-    check_val = round(N/2)
-#calling all my functions
-    # i shouldnt have this function in the loop
-    #im calculating alot xs i dont use, change it later to be more efficeint.
-    x, h = DIF(L,N)  
-    u_appx=HTAF(N,h,lamda,U_o,A)
-    #I bet I should define these so they are not in the function call.
-    N2=2*N
-    h2=L/(N2+1)
-    u_appx_next = HTAF(N2,h2,lamda,U_o,A)
-    # I still need to be comparing u values for the closest x points.  
-    if abs(u_appx[check_val]-u_appx_next[2*check_val+1])<Diff_N2N:
-        Flag = 1
-        print('%s Grid Points Needed' %(N))
-        print('Doubling the Grid Points would result in less then %s differnce between u values for the closest Grid Point' %(Diff_N2N))
-    else:
-        N=N+N   
-        
+        check_val = round(N/2)
+#calling my functions
+# i shouldnt have this function in the loop
+#im calculating alot xs i dont use, change it later to be more efficeint.
+        x, h = DIF(L,N)  
+        u_appx=HTAF(N,h,lamda,U_o,A)
+#I bet I should define these so they are not in the function call.
+        N2=2*N
+        h2=L/(N2+1)
+        u_appx_next = HTAF(N2,h2,lamda,U_o,A)
+# I still need to be comparing u values for the closest x points.  
+        if abs(u_appx[check_val]-u_appx_next[2*check_val+1])<Diff_N2N:
+            Flag = 1
+            print('%s Grid Points Needed' %(N))
+            print('Doubling the Grid Points would result in less then %s differnce between u values for the closest Grid Point \n' %(Diff_N2N))
+        else:
+            N=N+N   
+            
 #Note: here is the exact value function calls
-        
-u_exact = uEF(k, L, x, A, U_o)
-x2, h2= DIF(L, N2)
-u_exact_next = uEF(k, L, x2, A, U_o)   
-
+            
+    u_exact = uEF(k, L, x, A, U_o)
+    x2, h2= DIF(L, N2)
+    u_exact_next = uEF(k, L, x2, A, U_o)   
+    
 #formal order of accuracy.
-
+    
 #just keeping this quick method for now. G is still just a generic varibale name
-G1 = max([abs((u_appx[j]-u_exact[j])) for j in range(N)]) 
-G2 = max([abs((u_appx_next[j]-u_exact_next[j])) for j in range(2*N)])  
-fooa = log2(G1/G2)
-print(fooa)
-
+    G1 = max([abs((u_appx[j]-u_exact[j])) for j in range(N)]) 
+    G2 = max([abs((u_appx_next[j]-u_exact_next[j])) for j in range(2*N)])  
+    fooa = log2(G1/G2)
+    print(fooa)
+    
+#part two, Nuemann
+    
 #This is Now Mon 2.
+    
+    print('\nPart 2. Neumann Boundary conditions \n')
+    #grid convergenvce
+    N=N_initial
+    Flag = 0
+    while Flag == 0:
+    
+#comparing values near the middle of the interval now
+        check_val = round(N/2)
+#calling my functions
+        x, h = DIF(L,N)  
+        u2_appx=NHTAF(N, h, lamda, v, A)
+        #I bet I should define these so they are not in the function call.
+        N2=2*N
+        h2=L/(N2+1)
+        u2_appx_next = NHTAF(N2,h2,lamda, v, A)
+        # I still need to be comparing u values for the closest x points.  
+        if abs(u2_appx[check_val]-u2_appx_next[2*check_val+1])<Diff_N2N:
+            Flag = 1
+            print('%s Grid Points Needed' %(N))
+            print('Doubling the Grid Points would result in less then %s differnce between u values for the closest Grid Point \n' %(Diff_N2N))
+        else:
+            N=N+N   
+    
+#Note: here is the exact value function calls
+    u2_exact= uEF2(k, L, x, A, v)
+    x3, h3= DIF(L, N2)
+    u2_exact_next = uEF2(k, L, x3, A, U_o)   
+    
+#formal order of accuracy.
+    
+#just keeping this quick method for now. G is still just a generic varibale name
+    G3 = max([abs((u2_appx[j]-u2_exact[j])) for j in range(N-1)]) 
+    G4 = max([abs((u2_appx_next[j]-u2_exact_next[j])) for j in range(2*N-1)])  
+    fooa2 = log2(G3/G4)
+    print(fooa2)
